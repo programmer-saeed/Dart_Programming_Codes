859844a783e8005d4436c3dd86ea9d5913d9e0b3
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-01 00:43:47 +0600

    first commit

---
+++ untitled/bin/untitled.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+void main()
+{
+  // var x = 10;
+  // var y = 5;
+  // var z = x + y;
+  //
+  // print("The value of z is : $z");
+
+  // var mylist = ["Dhaka","Berlin","Helshaki","Paris","Amstradram","Oslo","Delhi","Islamabad","Kabul","Tehran","Riyad",12,15.6,true];
+  //
+  // print(mylist[13]);
+
+    // var person = {
+    //     'Name':'Saeed',
+    //      'Age':'24',
+    //     'City':'Dhaka',
+    //   'Height':'5 feet 6 inch'
+    // };
+    // print(person['Height']);
+
+  print("Hello, World");
+  string fisrtname = "Kazi Saeed";
+  string lastname = "Hossain";
+  string details = "Full name = $first"
+}
