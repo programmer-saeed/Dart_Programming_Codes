f294fda541ab88181d14508e3e1a2d09f69c8de0
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-03 19:01:45 +0600

    Added New Code

--- untitled/bin/Basic Syntax.dart
+++ untitled/bin/Basic Syntax.dart
@@ -1,8 +1,24 @@
 void main() {
 
-    var city = ["Dhaka","Rajshahi","Barishal","Chittagong","Sylhet","Khulna"];
+    var FirstNumber = 50;
+    print("First Number is  = $FirstNumber");
+
+    var SecondNumber = 20;
+    print("Second Number is = $SecondNumber");
+    
+    print("\n");
 
 
+    var Sum    = FirstNumber + SecondNumber;
+    var Sub    = FirstNumber - SecondNumber;
+    var Multi  = FirstNumber * SecondNumber;
+    var Div    = FirstNumber / SecondNumber;
+    var Mod    = FirstNumber % SecondNumber;
 
-  print(city[5]);
+    print("Summation is      = $Sum");
+    print("Subtraction is    = $Sub");
+    print("Multiplication is = $Multi");
+    print("Division is       = $Div");
+    print("Modulus is        = $Mod");
+ 
 }
---
+++ untitled/bin/Dart Module.dart
@@ -0,0 +1,6 @@
+void main (){
+
+  var City = ["Dhaka","Sylhet","Rangpur","Rajshahi","Khulna","Barishal","Comilla","Chittagong"];
+
+  print(City[0]);
+}
