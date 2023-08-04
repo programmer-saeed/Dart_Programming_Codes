822fa9b3d1c8d5a4c3597d0c3b8e631ca3b171b4
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-08-04 22:22:39 +0600

    Simple Coding Practice

--- Dart_Programming/bin/Dart Module.dart
+++ Dart_Programming/bin/Dart Module.dart
@@ -1,14 +1,24 @@
 void main (){
 
-  bool possitive = false;
-  print(possitive);
+  List<String> studentList = ["Saeed","Sadman","Raihan","Rafi"];
+  studentList.add("Fahim");
+  studentList.add("Amir");
+  studentList.addAll(["Shafi","Saon","Sumon","Abdul Ahad"]);
 
-  String firstName = "Kazi ";
-  String middileName = r"Saeed$ ";
-  String lastName = "Hossain";
-  String fullName = firstName + middileName + lastName;
+  studentList.remove("Abdul Ahad");
+  print(studentList[1]);
+  print(studentList.first);
+  print(studentList.last);
 
-  print(fullName);
+  List<double> studentResult = [];
+
+  studentResult.add(4.50);
+  studentResult.addAll([4.25,4.33,4.39,5.00,5.06]);
+  studentResult.remove(5.06);
+
+  print(studentResult[4]);
+  print(studentResult.first);
+  print(studentResult.last);
 
 
 }
